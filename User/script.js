e479fcbad35758b96b653c9656671911897eddd9
LeenAlsous <leen.amer22@hotmail.com>
2024-11-07 10:49:04 +0300

    js for health page, large screen only

--- User/script.js
+++ User/script.js
@@ -9,3 +9,18 @@ document.getElementById("proteinCalc").addEventListener("click", function (x) {
   x.preventDefault();
   document.getElementById("protein").style.display = "block";
 });
+
+document.getElementById("defaultTab").click();
+function showCalc(evt, calcId) {
+  var i, calc, option;
+  calc = document.getElementsByClassName("calc");
+  for (i = 0; i < calc.length; i++) {
+    calc[i].style.display = "none";
+  }
+  option = document.getElementsByClassName("option");
+  for (i = 0; i < option.length; i++) {
+    option[i].className = option[i].className.replace(" active", "");
+  }
+  document.getElementById(calcId).style.display = "block";
+  evt.currentTarget.className += " active";
+}
