f0b3b8e23b71b075623e47d3ba9517712f57189f
LeenAlsous <leen.amer22@hotmail.com>
2024-11-10 09:49:31 +0300

    adding restored files

---
+++ Admin/members.js
@@ -0,0 +1,10 @@
+
+    document.getElementById('popupmembers').addEventListener('click', function (x) {
+        x.preventDefault();
+        
+document.querySelector(".popupmm").style.visibility ="visible"
+})
+document.getElementById('close').addEventListener('submit', function (x) {
+    x.preventDefault();
+    
+document.querySelector(".popupmm").style.visibility ="hidden"})
