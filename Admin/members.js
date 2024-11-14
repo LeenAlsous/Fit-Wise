efbfbb4d799d1f4c7692fcd8c4755cbb7455cd27
yazanm02 <yazan.masafeh02@gmail.com>
2024-11-14 14:25:22 +0300

    reverting

--- Admin/members.js
+++ Admin/members.js
@@ -8,11 +8,7 @@ document.getElementById('close').addEventListener('submit', function (x) {
     x.preventDefault();
     
 document.querySelector(".popupmm").style.visibility ="hidden"})
-<<<<<<< HEAD
 ///////
-=======
-////
->>>>>>> 15-view-coaches-admin2
 function edit(id){
     document.getElementById(id).addEventListener('click', function (x) {
        x.preventDefault();
