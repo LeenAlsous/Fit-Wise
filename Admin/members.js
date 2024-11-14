26f07cf1692b3f7f3d16dafa5b006f60e5f5f622
yazanm02 <yazan.masafeh02@gmail.com>
2024-11-14 16:22:39 +0300

    a

--- Admin/members.js
+++ Admin/members.js
@@ -11,12 +11,12 @@ document.querySelector(".popupmm").style.visibility ="hidden"})
 
 /////////////////edit button
  function edit(id){
- document.getElementById(id).addEventListener('click', function (x) {
+
+     document.getElementById(id).addEventListener('click', function (x) {
     x.preventDefault();
-    
-document.querySelector(".popupmm").style.visibility ="visible"
+document.querySelector(".popupmmEdit").style.visibility ="visible"
 })
 document.getElementById('close').addEventListener('submit', function (x) {
 x.preventDefault();
 
-document.querySelector(".popupmm").style.visibility ="hidden"})}
+document.querySelector(".popupmmEdit").style.visibility ="hidden"})}
