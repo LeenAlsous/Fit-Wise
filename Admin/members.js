b674d7ab76115283fbabf2f8a547ed2cd261ce37
yazanm02 <yazan.masafeh02@gmail.com>
2024-11-14 13:53:30 +0300

    last2 page coach

--- Admin/members.js
+++ Admin/members.js
@@ -8,3 +8,14 @@ document.getElementById('close').addEventListener('submit', function (x) {
     x.preventDefault();
     
 document.querySelector(".popupmm").style.visibility ="hidden"})
+////
+function edit(id){
+    document.getElementById(id).addEventListener('click', function (x) {
+       x.preventDefault();
+       
+   document.querySelector(".popupmm").style.visibility ="visible"
+   })
+   document.getElementById('close').addEventListener('submit', function (x) {
+   x.preventDefault();
+   
+   document.querySelector(".popupmm").style.visibility ="hidden"})}
