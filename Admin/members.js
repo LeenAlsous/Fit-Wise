8020eedd38edbf50079cb29ab998a3a25cf6c9e4
yazanm02 <yazan.masafeh02@gmail.com>
2024-11-14 13:48:39 +0300

    last page members

--- Admin/members.js
+++ Admin/members.js
@@ -8,3 +8,15 @@ document.getElementById('close').addEventListener('submit', function (x) {
     x.preventDefault();
     
 document.querySelector(".popupmm").style.visibility ="hidden"})
+
+/////////////////edit button
+ function edit(id){
+ document.getElementById(id).addEventListener('click', function (x) {
+    x.preventDefault();
+    
+document.querySelector(".popupmm").style.visibility ="visible"
+})
+document.getElementById('close').addEventListener('submit', function (x) {
+x.preventDefault();
+
+document.querySelector(".popupmm").style.visibility ="hidden"})}
